4a23a8fecdca887323240363cf48531ed933ea94
liujun <522903052@qq.com>
2024-07-05 11:03:23 +0800

    fix

--- deploy/01-deploy-marketplace.js
+++ deploy/01-deploy-marketplace.js
@@ -1,24 +1,27 @@
-const hre = require("hardhat")
+const hre = require("hardhat");
 const { network } = require("hardhat");
 const { verify } = require("../utils/verify");
-async function main() {
-    console.log("deploy SecondHandVehicleMarketplace........")
-    const marketplace = await hre.ethers.deployContract("SecondHandVehicleMarketplace")
-    const contract = await marketplace.waitForDeployment();
-    console.log(await contract.getAddress())
-    console.log("SecondHandVehicleMarketplace deployed!")
 
-    if (network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
-        console.log("waiting for block confirmations....")
-        await contract.deploymentTransaction(6)
-        await verify(await contract.getAddress(), [])
-        // if not bytecode https://hardhat.org/hardhat-runner/docs/guides/verifying
-    }
+async function main() {
+  console.log("deploy SecondHandVehicleMarketplace........");
+  const marketplace = await hre.ethers.deployContract(
+    "SecondHandVehicleMarketplace"
+  );
+  const contract = await marketplace.waitForDeployment();
+  console.log(await contract.getAddress());
+  console.log("SecondHandVehicleMarketplace deployed!");
+
+  if (network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("waiting for block confirmations....");
+    await contract.deploymentTransaction(6);
+    await verify(await contract.getAddress(), []);
+    // if not bytecode https://hardhat.org/hardhat-runner/docs/guides/verifying
+  }
 }
 
-main().then(
-    () => process.exit(0)
-).catch((e) => {
-    console.error(e)
-    process.exit(1)
-})
+main()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,23 +2,23 @@
 // require("@nomiclabs/hardhat-etherscan")
 // require("hardhat-deploy")
 // require("solidity-coverage")
-require("hardhat-gas-reporter")
+require("hardhat-gas-reporter");
 // require("hardhat-contract-sizer")
 // require("@nomiclabs/hardhat-ethers");
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config()
+require("dotenv").config();
 // const chai = require('chai');
 // const eventemitter2 = require('chai-eventemitter2');
 
 // chai.use(eventemitter2());
 
-
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY_FOR_CONTRACT_ACCOUNT2 = process.env.PRIVATE_KEY_FOR_CONTRACT_ACCOUNT2
-const PRIVATE_KEY_ACCOUNT1 = process.env.PRIVATE_KEY_ACCOUNT1
-const PRIVATE_KEY_ACCOUNT3 = process.env.PRIVATE_KEY_ACCOUNT3
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY_FOR_CONTRACT_ACCOUNT2 =
+  process.env.PRIVATE_KEY_FOR_CONTRACT_ACCOUNT2;
+const PRIVATE_KEY_ACCOUNT1 = process.env.PRIVATE_KEY_ACCOUNT1;
+const PRIVATE_KEY_ACCOUNT3 = process.env.PRIVATE_KEY_ACCOUNT3;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -31,7 +31,11 @@ module.exports = {
     },
     sepolia: {
       url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY_FOR_CONTRACT_ACCOUNT2, PRIVATE_KEY_ACCOUNT1, PRIVATE_KEY_ACCOUNT3],
+      accounts: [
+        PRIVATE_KEY_FOR_CONTRACT_ACCOUNT2,
+        PRIVATE_KEY_ACCOUNT1,
+        PRIVATE_KEY_ACCOUNT3,
+      ],
       chainId: 11155111,
       blockConfirmations: 6,
     },
@@ -39,7 +43,7 @@ module.exports = {
   etherscan: {
     apiKey: {
       sepolia: ETHERSCAN_API_KEY,
-    }
+    },
   },
   solidity: {
     version: "0.8.19",
@@ -63,7 +67,6 @@ module.exports = {
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
 };
